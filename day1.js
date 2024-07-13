79b6962b13a16db61a5d429c29fdbe4bc43b372b
KonArchit <archit483@gmail.com>
2024-07-13 12:48:20 +0530

    Day 1 of JS Challenge

---
+++ day1.js
@@ -0,0 +1,46 @@
+// Task 1:
+var a = 11
+console.log(a)
+
+// Task 2:
+let b = "archit"
+console.log(b)
+
+// Task 3:
+const c = true
+console.log(c)
+
+// Task 4:
+let num = 31
+console.log(typeof num)
+
+let str = "Harsh"
+console.log(typeof str)
+
+let bool = false
+console.log(typeof bool)
+
+let obj = {
+    name: "Agarwal ji",
+    rollno: 2000650100011,
+    age: 22,
+}
+console.log(typeof obj)
+
+let arr = ["22","archit"]
+console.log(typeof arr)
+// How to know if a variable is an array?
+// ECMAScript 5 (2009) defined a new method for this: Array.isArray():
+console.log(Array.isArray(arr))
+
+// Task 5:
+let x = 10
+console.log(x)
+x = 11
+console.log(x)
+
+// Task 6:
+const understanding = 100
+understanding = 111
+console.log(understanding)
+// The TypeError is showing in the console
